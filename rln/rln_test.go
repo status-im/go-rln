c53778e7980ca25041d3a14d168455b54d41b897
Richard Ramos <info@richardramos.me>
2022-06-30 18:12:18 -0400

    refactor: make API match nwaku

--- rln/rln_test.go
+++ rln/rln_test.go
@@ -2,7 +2,6 @@ package rln_test
 
 import (
 	"encoding/hex"
-	"io/ioutil"
 	"reflect"
 	"testing"
 
@@ -10,53 +9,38 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	params, err := ioutil.ReadFile("./testdata/parameters.key")
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	_, err = rln.New(32, params)
+	_, err := rln.NewRLNWithDepth(32)
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestGenerateKey(t *testing.T) {
-	params, err := ioutil.ReadFile("./testdata/parameters.key")
+	r, err := rln.NewRLNWithDepth(32)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	r, err := rln.New(32, params)
+	k, err := r.MembershipKeyGen()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	k, err := r.GenerateKey()
-	if err != nil {
-		t.Fatal(err)
+	if reflect.DeepEqual(k.IDKey, [32]byte{}) {
+		t.Fatal("k.IDKey was empty")
 	}
 
-	if reflect.DeepEqual(k.Key, [32]byte{}) {
-		t.Fatal("k.Key was empty")
-	}
-
-	if reflect.DeepEqual(k.Commitment, [32]byte{}) {
-		t.Fatal("k.Commitment was empty")
+	if reflect.DeepEqual(k.IDCommitment, [32]byte{}) {
+		t.Fatal("k.IDCommitment was empty")
 	}
 }
 
-/*
 func TestRLN_Hash(t *testing.T) {
 	// This test is based on tests from:
 	// https://github.com/status-im/nim-waku/blob/b7998de09d1ef04599a699938da69aecfa63cc6f/tests/v2/test_waku_rln_relay.nim#L527
 
-	params, err := ioutil.ReadFile("./testdata/parameters.key")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	r, err := rln.New(32, params)
+	r, err := rln.NewRLNWithDepth(32)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -68,37 +52,32 @@ func TestRLN_Hash(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	expected := "53a6338cdbf02f0563cec1898e354d0d272c8f98b606c538945c6f41ef101828"
-	if expected != hex.EncodeToString(output) {
+	expected := "9b581442bb7fe1bc29b5cfbc29377c0b080549bcd902d1290288d93fc773bb20"
+	if expected != hex.EncodeToString(output[:]) {
 		t.Fatalf("value %x did not match expected %s", output, expected)
 	}
-}*/
+}
 
 func TestRLN_GetRoot(t *testing.T) {
 	// This test is based on tests from:
 	// https://github.com/status-im/nim-waku/blob/b7998de09d1ef04599a699938da69aecfa63cc6f/tests/v2/test_waku_rln_relay.nim#L320
 
-	params, err := ioutil.ReadFile("./testdata/parameters.key")
+	r, err := rln.NewRLNWithDepth(32)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	r, err := rln.New(32, params)
+	root1, err := r.GetMerkleRoot()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	root1, err := r.GetRoot()
+	root2, err := r.GetMerkleRoot()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	root2, err := r.GetRoot()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if hex.EncodeToString(root1) != hex.EncodeToString(root2) {
+	if hex.EncodeToString(root1[:]) != hex.EncodeToString(root2[:]) {
 		t.Fatalf("value %x did not match expected %x", root1, root2)
 	}
 }
